bda1a37dc517e8c93e811ab82c68543983ea962f
MatheusSouzaRodrigues235 <matheus.rodrigues245@etec.sp.gov.br>
2022-03-09 16:47:09 -0300

    ImplementandoApiViaCep,PegandoTokenCartao

--- screens/compras/cart/template_store/checkout.php
+++ screens/compras/cart/template_store/checkout.php
@@ -1,7 +1,21 @@
 <?php
 	session_start();
-	include_once 'head.html';
 	
+	// pegando session ID
+	$url = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions";
+
+	$credenciais = array(
+	    	"email" => "edersjc1000@gmail.com",
+			"token" => "6306914EF35C4FF1B4EB8359CDFE1DF8"
+	);
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($credenciais));
+	$resultado = curl_exec($curl);
+	curl_close($curl);
+	$session = simplexml_load_string($resultado)->id;
+
 	require __DIR__ . '/../DataBase/connection.php';
 	require __DIR__ . '/../App/Controller/ClienteController.php';
 
@@ -38,169 +52,296 @@
 		header('Location: cart.php');
 	}
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Checkout example · Bootstrap v5.1</title>
 
-<!DOCTYPE HTML>
-<html>
-	<body>
-		
-	<div class="colorlib-loader"></div>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/checkout/">
 
-	<div id="page">
-		<nav class="colorlib-nav" role="navigation">
-			<div class="top-menu">
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-2">
-							<div id="colorlib-logo"><a href="index.php">Use New Mic</a></div>
-						</div>
-						<div class="col-xs-10 text-right menu-1">
-							<ul>
-								<li><a href="index.php">Home</a></li>
-								<li><a href="shop.php">Produtos</a></li>
-								<?php
-									if ($result == true) {
-										echo '<li><a href="list.php"> Seus Produtos </a></li>';
-										echo '
-											<li class="active"><a href="cart.php"><i class="icon-shopping-cart"></i> Carrinho </a></li>
-											<li><a href="../App/Controller/logout.php"> Sair </a></li>';
-									}else{
-										echo '<li><a href="login.php"> Login/Cadastre-se </a></li>';
-									}
-								?>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
-		<aside id="colorlib-hero" class="breadcrumbs">
-			<div class="flexslider">
-				<ul class="slides">
-			   	<li style="background-image: url(images/3.jpg);">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
-				   				<div class="slider-text-inner text-center">
-				   					<h1>Pagamento</h1>
-				   					<h2 class="bread"><span><a href="index.php">Home</a></span> <span><a href="cart.php">Carrinho de compras</a></span> <span>Pagamento</span></h2>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
-			  	</ul>
-		  	</div>
-		</aside>
+    
 
-		<div class="colorlib-shop">
-			<div class="container">
-				<div class="row row-pb-md">
-					<div class="col-md-10 col-md-offset-1">
-						<div class="process-wrap">
-							<div class="process text-center active">
-								<p><span>01</span></p>
-								<h3>Carrinho de Compras</h3>
+    <!-- Bootstrap core CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      <h2>Checkout</h2>
+      <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+    </div>
+
+    <div class="row g-5">
+      <div class="col-md-5 col-lg-4 order-md-last">
+        <h4 class="d-flex justify-content-between align-items-center mb-3">
+          <span class="text-primary">Your cart</span>
+          <span class="badge bg-primary rounded-pill">3</span>
+        </h4>
+        <ul class="list-group mb-3">
+		  <?php 
+					
+					
+					
+					$count = 0;
+					$total = 0;
+					foreach( $resultado_carrinho as $row ) { 
+						$count = $row[1]*$row[4];
+						$total = $count + $total;
+						echo '
+						<li class="list-group-item d-flex justify-content-between lh-sm">
+							<div>
+							<h6 class="my-0">'.$row[4].'x'.$row[0].' </h6>
+							<span>R$ '.number_format($row[1]*$row[4],2,",",".").'</span>
 							</div>
-							<div class="process text-center active">
-								<p><span>02</span></p>
-								<h3>Pagamento</h3>
-							</div>
-							<div class="process text-center">
-								<p><span>03</span></p>
-								<h3>Finalizado</h3>
-							</div>
-						</div>
-					</div>
-				</div>
-				<form method="post" action="../App/Controller/insertFinalizado.php" class="colorlib-form">
-				<div class="row">
-					<div class="col-md-7">
-						
-							<h2>Endereço para entrega</h2>
-			              	<div class="row">
-				               <div class="col-md-12">
-				                  <div class="form-group">
-				                  	<label for="country">Estado</label>
-				                    <div class="form-field">
-				                     	<i class="icon icon-arrow-down3"></i>
-				                     	<select class="form-control" name="id_estado" id="id_estado" required>
-											<option value=""> Selecione...</option>
-											 
-												<?php foreach( $resultado_estados as $row ) { 
-													echo '<option value="'.$row['Uf'].'">'.$row['Nome'].'</option>';
-												} ?>
+							<span class="text-muted"> '.number_format($total,2,",",".").'</span>
+						</li>';
+					}
+					
+					?>
+        </ul>
 
-										</select>			                 
-				                    </div>
-				                </div>
-				                  <div class="form-group">
-				                  	<label for="id_cidade">Municipio</label>
-				                     <div class="form-field">
-				                     	<i class="icon icon-arrow-down3"></i>
-				                     	<select class="form-control" name="id_cidade" id="id_cidade" required>
-											<option value="#id_cidade">Selecione...</option>
-										</select>			                 
-				                     </div>
+        <form class="card p-2">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Promo code">
+            <button type="submit" class="btn btn-secondary">Redeem</button>
+          </div>
+        </form>
+      </div>
+      <div class="col-md-7 col-lg-8">
+        <h4 class="mb-3">Billing address</h4>
+        <form class="needs-validation" novalidate>
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="NomeCompleto" class="form-label">First name</label>
+              <input type="text" class="form-control" id="NomeCompleto" placeholder="Primeiro Nome" value="" required>
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
 
-				                    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-									<script type="text/javascript">
-										$('#id_estado').change(function (){
-											var valor = document.getElementById("id_estado").value;
-											$.get("exibe_cidade.php?search=" + valor, function (data) {
-												$("#id_cidade").find("option").remove();
-												$('#id_cidade').append(data);
-											});
-										});
-									</script>
-				                  </div>
-				               </div>
-				            </div>
-		            	
-					</div>
-					<div class="col-md-5">
-						<div class="cart-detail">
-							<h2>Total</h2>
-							<ul>
-								<?php 
-									$count = 0;
-									$total = 0;
-									foreach( $resultado_carrinho as $row ) { 
-										$count = $row[1]*$row[4];
-										$total = $count + $total;
-										echo '
-										<li>
-											<ul>
-												<li><span>'.$row[4].' x '.$row[0].'</span> <span>R$ '.number_format($row[1]*$row[4],2,",",".").'</span></li>
-											</ul>
-										</li>';
-									}
-										echo '<li><span>Total</span> <span>R$ '.number_format($total,2,",",".").'</span></li>';
-									?>
-							</ul>
-						</div>
-						
-						<div class="row">
-							<div class="col-md-12">
-								<p><input type="submit" style="background-color: green !important" class="btn btn-primary" value="Finalizar" name="enviar"></input></p>
-								<p><a href="cart.php" class="btn btn-primary">Voltar para o carrinho </a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</form>
-			</div>
-		</div>
+			<div class="col-12">
+              <label for="cpf" class="form-label">CPF</label>
+              <div class="input-group has-validation">
+                <span class="input-group-text">@</span>
+                <input type="text" class="form-control" id="cpf" placeholder="CPF" required maxlength="11">
+              <div class="invalid-feedback">
+                  Your username is required.
+                </div>
+              </div>
+            </div>
 
-		<?php
-			include_once 'footer.html';
-		?>
-	</div>
+            <div class="col-12">
+              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
+              <input type="email" class="form-control" id="email" placeholder="you@example.com">
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
 
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
-	</div>
+			<div class="col-12">
+              <label for="Telefone" class="form-label">Telefone </label>
+              <input type="tel" class="form-control" id="Telefone" required>
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+			<div class="col-3">
+              <label for="cepEndec" class="form-label">CEP</label>
+              <input type="text" class="form-control" id="cepEndec" placeholder="CEP" required>
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+            <div class="col-12">
+              <label for="endereco" class="form-label">Endereco</label>
+              <input type="text" class="form-control" id="endereco" placeholder="1234 Main St" required>
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div class="col-3">
+              <label for="numero" class="form-label">Numero</label>
+              <input type="text" class="form-control" id="numero" placeholder="Apartment or suite">
+            </div>
+
+			<div class="col-12">
+              <label for="bairroEnd" class="form-label">Bairro</label>
+              <input type="text" class="form-control" id="bairroEnd" placeholder="Apartment or suite">
+            </div>
+
+            <div class="col-md-5">
+              <label for="cidadeEnd" class="form-label">Cidade</label>
+              <input type="text" class="form-control" id="cidadeEnd" placeholder="Apartment or suite">
+            
+              <div class="invalid-feedback">
+                Please select a valid country.
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <label for="estadoEnd" class="form-label">Estado</label>
+              <input type="text" class="form-control" id="estadoEnd" placeholder="Apartment or suite">
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+
+           
+
+          <hr class="my-4">
+
+          <h4 class="mb-3">Payment</h4>
+            <div class="col-md-6">
+              <label for="creditCardNumber" class="form-label">Credit card number</label>
+              <input type="text" class="form-control" id="creditCardNumber" placeholder="" required name="creditCardNumber">
+              <div class="invalid-feedback">
+                Credit card number is required
+              </div>
+            </div>
+
+			<div class="col-md-3">
+              <label for="creditCardBrand" class="form-label">Bandeira</label>
+              <input type="text" class="form-control" id="creditCardBrand" placeholder="" required name="creditCardBrand">
+              <div class="invalid-feedback">
+                Expiration date required
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="creditCardExpMonth" class="form-label">Mês Validade</label>
+              <input type="text" class="form-control" id="creditCardExpMonth" placeholder="" required name="creditCardExpMonth">
+              <div class="invalid-feedback">
+                Expiration date required
+              </div>
+            </div>
+
+			<div class="col-md-3">
+              <label for="creditCardExpYear" class="form-label">Ano Validade</label>
+              <input type="text" class="form-control" id="creditCardExpYear" placeholder="" required name="creditCardExpYear">
+              <div class="invalid-feedback">
+                Expiration date required
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="creditCardCvv" class="form-label">CVV</label>
+              <input type="text" class="form-control" id="creditCardCvv" placeholder="" required name="creditCardCvv">
+              <div class="invalid-feedback">
+                Security code required
+              </div>
+            </div>
+          </div>
+
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+        </form>
+      </div>
+    </div>
+  </main>
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2017–2021 Company Name</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div>
+
+
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+      <script src="form-validation.js"></script>
+	  <script>
 	
-	</body>
-</html>
+	'use strict';
+	//APi ViaCep para pesquisa de endereço dinâmico
+	const preencherFormulario = (endereco) =>{
+		document.getElementById('endereco').value = endereco.logradouro;
+		document.getElementById('bairroEnd').value = endereco.bairro;
+		document.getElementById('cidadeEnd').value = endereco.localidade;
+		document.getElementById('estadoEnd').value = endereco.uf;
+	}
+	const pesquisarCep = async() => {
+	const cep = document.getElementById('cepEndec').value;
+	const url = 'https://viacep.com.br/ws/'+cep+'/json/';
+	const dados = await fetch(url);
+	const endereco = await dados.json();
+	preencherFormulario(endereco);
 
+}
+
+	document.getElementById('cepEndec')
+        .addEventListener('focusout',pesquisarCep);
+
+	
+	</script>
+
+	<script type="text/javascript" src= "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js" > </script>
+	<script type="text/javascript" src="./js/JSA.js" > </script>
+	<script type="text/javascript" >
+			PagSeguroDirectPayment.setSessionId('<?php echo $session; ?>');
+	console.log('<?php echo $session; ?>');
+
+
+	$("#creditCardNumber").keyup(function(){
+		if ($("#creditCardNumber").val().length >= 6) {
+			PagSeguroDirectPayment.getBrand({
+				cardBin: $("#creditCardNumber").val().substring(0,6),
+				success: function(response) { 
+						console.log(response);
+						$("#creditCardBrand").val(response['brand']['name']);
+						$("#creditCardCvv").attr('size', response['brand']['cvvSize']);
+				},
+				error: function(response) {
+					console.log(response);
+				}
+			})
+		};
+	})
+	
+
+	</script>
+	
+  </body>
+</html>
