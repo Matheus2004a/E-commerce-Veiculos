b9e9faffbde453df716cf7829e1db08470f06825
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-03-16 17:18:24 -0300

    Centralizando conteúdo e ajustando tamanho da imagem do mapa

--- screens/home/mapa_site.php
+++ screens/home/mapa_site.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mapa do Site</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <style>
-        .principal{
-            text-align: center;
-            font-weight: bold;
-            color: green;
-        }
-    </style>
+    <link rel="stylesheet" href="./css/map.css">
+    <link rel="stylesheet" href="../../bootstrap-5.1.3-dist/css/bootstrap.min.css">
 </head>
+
 <body>
-        <h1 class="principal">Mapa do Site</h1>
-        <p class="principal">Mapa para se referenciar dentro do sistema</p>
-    <div class="mapa">
-        <img src="./assets/images/Mapa do site.png" alt="">
-    </div>
-    <a href="../../index.php"><button style="background-color:#93E1FA; font-weight:bold;border:1px;">Pagina Principal</button> </a>
+    <main class="container align-items-center">
+        <h1 class="principle">Mapa do Site</h1>
+        <p class="principle">Mapa para se referenciar dentro do sistema</p>
+        <figure>
+            <img src="./assets/images/Mapa do site.png" alt="map-site" class="map">
+        </figure>
+        <a href="../../index.php">
+            <button type="button" class="btn btn-secondary">Home</button>
+        </a>
+    </main>
 </body>
+
 </html>
