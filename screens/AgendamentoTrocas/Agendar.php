c9b27091958bc67ed7dcf3873b9ff14955ad946d
ImBard <nicota1234@gmail.com>
2022-04-12 17:25:06 -0300

    side bar e header tela agendamento

--- screens/AgendamentoTrocas/Agendar.php
+++ screens/AgendamentoTrocas/Agendar.php
@@ -6,14 +6,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendar Serviços</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../home/css/style.css">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="./style/style.css">
+    
 </head>
 
 <body>
+    <header>
+        <img src="../../images/icones/brand header.png" class="brand">
+        <!-- <a class="brand" href="#">Logomarca</a> !-->
+        <nav class="menus">
+            <ul>
+                <li>
+                    <a class="active" aria-current="page" href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#services">Todos os serviços</a>
+                </li>
+                <li>
+                    <a href="#about">Sobre</a>
+                </li>
+                <li>
+                    <a href="#contact">Contate - nos</a>
+                </li>
+                <li>
+                    <a href="./screens/home/mapa_site.php">Mapa do Site</a>
+                </li>
+                <li>
+                    <a href="./screens/login/login.php">Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
     <nav class="sidebar close">
         <header>
             <div class="image-text">
@@ -102,80 +126,51 @@
 
     </nav>
 
-    <header>
-        <img src="../../images/icones/brand header.png" class="brand">
-        <!-- <a class="brand" href="#">Logomarca</a> !-->
-        <nav class="menus">
-            <ul>
-                <li>
-                    <a class="active" aria-current="page" href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#services">Todos os serviços</a>
-                </li>
-                <li>
-                    <a href="#about">Sobre</a>
-                </li>
-                <li>
-                    <a href="#contact">Contate - nos</a>
-                </li>
-                <li>
-                    <a href="./screens/home/mapa_site.php">Mapa do Site</a>
-                </li>
-                <li>
-                    <a href="./screens/login/login.php">Login</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <!-- Div container, perfil e botões chat e cancelar -->
-    <div class="container">
-        <div class="perfil">
-            <img src="./Images/Ellipse 15.png" alt="">
-            <div class="spanPerfil">
-                <span id="nomeSpan">Luas Azevedo</span>
-                <span id="descSpan">Mecânico para motor</span>
+    <div class="home">
+        <div class="container">
+            <div class="perfil">
+                <img src="./Images/Ellipse 15.png" alt="">
+                <div class="spanPerfil">
+                    <span id="nomeSpan">Luas Azevedo</span>
+                    <span id="descSpan">Mecânico para motor</span>
+                </div>
+                <button>
+                    <img src="./Images/close.png" alt="">
+                </button>
             </div>
-            <button>
-                <img src="./Images/close.png" alt="">
-            </button>
+
+            <div class="buttonContainer">
+                <button type="submit" class="btn btn-secondary btn-lg">Chat</button>
+                <button type="submit" class="btn btn-secondary btn-lg">Cancelar</button>
+            </div>
         </div>
 
-        <div class="buttonContainer">
-            <button type="submit" class="btn btn-secondary btn-lg">Chat</button>
-            <button type="submit" class="btn btn-secondary btn-lg">Cancelar</button>
+        <div class="calendario">
+            <input type="date" name="date" id="date" min="2022-04-12" max="2022-04-22">
+            <span>Lorem Ipsum is simply dummy text of the printing and typ
+                esetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+        </div>
+
+        <div class="horarios">
+
+            <input type="button" value="09:15">
+            <input type="button" value="09:50">
+            <input type="button" value="10:40">
+            <input type="button" value="12:15">
+            <input type="button" value="12:45">
+            <input type="button" value="14:45">
+            <input type="button" value="15:25">
+            <input type="button" value="16:15">
+            <input type="button" value="17:00">
+
+            <div class="observacoes">
+                <span>Observações</span>
+                <textarea class="form-control" cols="10" rows="5"></textarea>
+            </div>
+            <input id="btnConfirm" type="button" value="Confirmar">
         </div>
     </div>
-    <!-- Fim da div Container-->
-
-    <!-- Inicio div Calendario e datas -->
-    <div class="calendario">
-        <input type="date" name="date" id="date" min="2022-04-12" max="2022-04-22">
-        <span>Lorem Ipsum is simply dummy text of the printing and typ
-            esetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-    </div>
-    <!-- Fim div calendario e datas -->
-
-    <div class="horarios">
-
-        <input type="button" value="09:15">
-        <input type="button" value="09:50">
-        <input type="button" value="10:40">
-        <input type="button" value="12:15">
-        <input type="button" value="12:45">
-        <input type="button" value="14:45">
-        <input type="button" value="15:25">
-        <input type="button" value="16:15">
-        <input type="button" value="17:00">
-
-        <div class="observacoes">
-            <span>Observações</span>
-            <textarea class="form-control" cols="10" rows="5"></textarea>
-        </div>
-        <input id="btnConfirm" type="button" value="Confirmar">
-    </div>
-
     <script>
         const body = document.querySelector('body'),
             sidebar = body.querySelector('nav'),
