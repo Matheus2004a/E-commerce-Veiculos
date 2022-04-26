2a28787310dcd6751fc1390b7016d39bb1838819
ImBard <nicota1234@gmail.com>
2022-04-26 13:44:31 -0300

    tela cadastro de serviços

---
+++ screens/CadastrarServicos/CadastrarServico.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleServico.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <p>Agende seus serviços</p>
+        <div class="divTop">
+            <div class="left">
+                <div class="divTextServicoLeft">
+                    <h3>Tipos de serviços</h3>
+                    <p>Escolha abaixo qual tipo de serviço você quer cadastrar </p>
+                </div>
+
+                <div class="divTipoServicoLeft">
+                    <div class="checkItem">
+                        <input type="checkbox" name="" id=""> <span>Troca de óleo</span>
+                    </div>
+                    <div class="checkItem">
+                        <input type="checkbox" name="" id=""> <span>Troca de óleo</span>
+                    </div>
+                    <div class="checkItem">
+                        <input type="checkbox" name="" id=""> <span>Troca de óleo</span>
+                    </div>
+                    <div class="checkItem">
+                        <input type="checkbox" name="" id=""> <span>Troca de óleo</span>
+                    </div>
+                    <div class="checkItem">
+                        <input type="checkbox" name="" id=""> <span>Troca de óleo</span>
+                    </div>
+                    <div class="checkItem">
+                        <input type="checkbox" name="" id=""> <span>Troca de óleo</span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="right">
+                <div class="perfil">
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class=divBot>
+            div 2
+        </div>
+        <button>teste</button>
+    </div>
+</body>
+
+</html>
