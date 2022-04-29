3155205219aaa169257d653aa06d05ef9c4a17ef
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-04-28 23:55:55 -0300

    :sparkles: feat: Adiciona mais campos ao form de cadastro de produtos

--- index.php
+++ index.php
@@ -11,13 +11,14 @@
 
 <body>
   <a name="top-page"></a>
+  
   <header>
     <img src="./images/icones/brand header.png" class="brand">
     <!-- <a class="brand" href="#">Logomarca</a> !-->
     <nav class="menus">
       <ul>
         <li>
-          <a class="active" aria-current="page" href="#">Home</a>
+          <a href="#home">Home</a>
         </li>
         <li>
           <a href="#services">Todos os serviços</a>
@@ -34,7 +35,6 @@
       </ul>
     </nav>
   </header>
-
 
   <main>
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
--- screens/register-products/index.php
+++ screens/register-products/index.php
@@ -12,85 +12,103 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de produtos</title>
     <link rel="stylesheet" href="../../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../home/css/style.css">
 </head>
 
 <?php
 require __DIR__ . '/../../components/messages-alerts/icons.php';
 ?>
 
-<body>
-    <main class="container">
-        <h2 class="text-center">Cadastrar produto</h2>
-        <form action="upload.php" method="post" enctype="multipart/form-data" class="row g-3 needs-validation" novalidate>
-            <?php
-            if (isset($_SESSION['register-product'])) {
-                echo $_SESSION['register-product'];
-                unset($_SESSION['register-product']);
-            } else if (isset($_SESSION['no-register-product'])) {
-                echo $_SESSION['no-register-product'];
-                unset($_SESSION['no-register-product']);
-            }
-            ?>
-            <div class="col-md-6">
-                <label for="validationCustom01" class="form-label">Nome do produto</label>
-                <input type="text" class="form-control" name="name-product" id="validationCustom01" required autofocus>
-                <div class="invalid-feedback">
-                    Digite o nome do produto.
-                </div>
-            </div>
+<body class="hold-transition register-page">
+    <?php
+        include "../../components/header.php";
+    ?>
 
-            <div class="col-md-6">
-                <label for="validationCustom04" class="form-label">Categoria do produto</label>
-                <select class="form-select" name="category-product" id="validationCustom04" required>
-                    <option selected disabled value="">Selecione sua categoria</option>
-                    <option value="Rodas">Rodas</option>
-                    <option value="Filtros de ar">Filtros de ar</option>
-                    <option value="Óleo">Óleo</option>
-                </select>
-                <div class="invalid-feedback">
-                    Digite a categoria do produto.
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <label for="validationCustom03" class="form-label">Cód do produto</label>
-                <input type="text" class="form-control" name="cod-product" id="validationCustom03" required>
-                <div class="invalid-feedback">
-                    Digite o código do produto.
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <label for="validationCustom05" class="form-label">Preço</label>
-                <input type="text" class="form-control" name="price-product" id="validationCustom05" required>
-                <div class="invalid-feedback">
-                    Digite o preço do produto.
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <label for="validationCustom05" class="form-label">Descrição do produto</label>
-                <textarea type="text" class="form-control" name="describe-product" id="validationCustom05" required></textarea>
-                <div class="invalid-feedback">
-                    Digite a descrição do produto.
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <label for="validationCustom05" class="form-label">Foto do produto</label>
-                <input type="file" name="file" class="form-control" id="validationCustom05" required>
-                <div class="invalid-feedback">
-                    Insira a foto do produto.
+    <main class="container card">
+        <div class="card-body register-card-body">
+            <h2 class="text-center mb-4">Cadastrar produto</h2>
+            <form action="upload.php" method="post" enctype="multipart/form-data" class="row g-3 needs-validation" novalidate>
+                <?php
+                if (isset($_SESSION['register-product'])) {
+                    echo $_SESSION['register-product'];
+                    unset($_SESSION['register-product']);
+                } else if (isset($_SESSION['no-register-product'])) {
+                    echo $_SESSION['no-register-product'];
+                    unset($_SESSION['no-register-product']);
+                }
+                ?>
+                <div class="col-md-6">
+                    <label for="validationCustom01" class="form-label">Nome do produto</label>
+                    <input type="text" class="form-control" name="name-product" id="validationCustom01" required autofocus>
+                    <div class="invalid-feedback">
+                        Digite o nome do produto.
+                    </div>
                 </div>
 
-                <img alt="" id="img ">
-            </div>
+                <div class="col-md-6">
+                    <label for="validationCustom04" class="form-label">Categoria do produto</label>
+                    <select class="form-select" name="category-product" id="validationCustom04" required>
+                        <option selected disabled value="">Selecione sua categoria</option>
+                        <option value="Rodas">Rodas</option>
+                        <option value="Filtros de ar">Filtros de ar</option>
+                        <option value="Óleo">Óleo</option>
+                    </select>
+                    <div class="invalid-feedback">
+                        Digite a categoria do produto.
+                    </div>
+                </div>
 
-            <div class="col-12">
-                <button class="btn btn-primary" type="submit">Cadastrar produto</button>
-            </div>
-        </form>
+                <div class="col-md-6">
+                    <label for="validationCustom05" class="form-label">Preço</label>
+                    <input type="text" class="form-control" name="price-product" id="validationCustom05" required>
+                    <div class="invalid-feedback">
+                        Digite o preço do produto.
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <label for="validationCustom01" class="form-label">Fabricante</label>
+                    <input type="text" class="form-control" name="name-product" id="validationCustom01" required autofocus>
+                    <div class="invalid-feedback">
+                        Digite o nome do produto.
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <label for="validationCustom01" class="form-label">Qtd em estoque</label>
+                    <input type="number" class="form-control" name="name-product" id="validationCustom01" required autofocus>
+                    <div class="invalid-feedback">
+                        Digite o nome do produto.
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <label for="validationCustom05" class="form-label">Descrição do produto</label>
+                    <textarea type="text" class="form-control" name="describe-product" id="validationCustom05" required></textarea>
+                    <div class="invalid-feedback">
+                        Digite a descrição do produto.
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <label for="validationCustom05" class="form-label">Foto do produto</label>
+                    <input type="file" name="file" class="form-control" id="validationCustom05" required>
+                    <div class="invalid-feedback">
+                        Insira a foto do produto.
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <a href="">
+                        <button class="btn btn-secondary">Voltar</button>
+                    </a>
+                    <button class="btn btn-primary" type="submit">Cadastrar produto</button>
+                </div>
+            </form>
+        </div>
     </main>
 
     <script src="./js/main.js"></script>
