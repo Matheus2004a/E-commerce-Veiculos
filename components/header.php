374bfa7f300ffd011378a14952e30577a1cd6ef3
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-04-28 23:44:47 -0300

    :sparkles: feat: Cria componente header

---
+++ components/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <header>
+        <img src="./images/icones/brand header.png" class="brand">
+        <!-- <a class="brand" href="#">Logomarca</a> !-->
+        <nav class="menus">
+            <ul>
+                <li>
+                    <a class="active" aria-current="page" href="../../index.php">Home</a>
+                </li>
+                <li>
+                    <a href="#services">Todos os serviços</a>
+                </li>
+                <li>
+                    <a href="#about">Sobre</a>
+                </li>
+                <li>
+                    <a href="#contact">Contate - nos</a>
+                </li>
+                <li>
+                    <a href="../../screens/login/login.php">Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
