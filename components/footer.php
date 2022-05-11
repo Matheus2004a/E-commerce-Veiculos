4ba9f4ccd460e47a795d1ef58685f7a072661970
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-05-11 17:25:18 -0300

    :zap: perf: Cria componente footer

---
+++ components/footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <footer class="footer-blog">
+        <div class="list-ordenate">
+            <h5>Contatos</h5>
+            <a href="">Lorem Ipsum</a>
+            <a href="">Lorem Ipsum</a>
+            <a href="">Lorem Ipsum</a>
+        </div>
+
+        <div class="list-ordenate">
+            <h5>Suporte</h5>
+            <a href="">Lorem Ipsum</a>
+            <a href="">Lorem Ipsum</a>
+            <a href="">Lorem Ipsum</a>
+        </div>
+
+        <div class="list-ordenate">
+            <h5>Serviços</h5>
+            <a href="">Lorem Ipsum</a>
+            <a href="">Lorem Ipsum</a>
+            <a href="">Lorem Ipsum</a>
+        </div>
+
+        <div class="list-ordenate">
+            <h5>Pagamentos</h5>
+            <ul>
+                <li>
+                    <figure>
+                        <img src="../images/american.png" alt="icon-card-american">
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="../images/paypal.png" alt="icon-card-paypal">
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="../images/mastercard.png" alt="icon-card-mastercard">
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="../images/pago.png" alt="icon-card-pago">
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <img src="../images/boleto.png" alt="icon-card-boleto">
+                    </figure>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</body>
+
+</html>
