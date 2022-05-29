1cd2eb371bf9376a83bfdb0f2adfcdccf07bcab6
MatheusSouzaRodrigues235 <matheus.rodrigues245@etec.sp.gov.br>
2022-05-28 21:53:48 -0300

    :feat: Requisicao api CalcPrazo

--- screens/compras/cart/template_store/calcular.php
+++ screens/compras/cart/template_store/calcular.php
@@ -1,21 +1,13 @@
 <?php
-    require __DIR__.'/../../../../vendor/autoload.php';
-    use \App\WebSerice\Correios;
-
-    $obCorreios = new Correios();
-
-    $codigoServico;
-    $cepOrigem;
-    $cepDestino;
-    $peso;
-    $formato;
-    $comprimento;
-    $altura;
-    $largura;
-    $diametro=0;
-    $maoPropria=false;
-    $valorDeclarado=0;
-    $avisoRecebimento=false;
-
-    $frete = $obCorreios -> calcularFrete();
+    $variaveis_extras = http_build_query($_POST);
+    $url = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&sCdAvisoRecebimento=n&sCdMaoPropria=n&nVlValorDeclarado=0&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3&nCdFormato=1&" . $variaveis_extras;
+    
+    $unparsedResult = file_get_contents($url);
+    $parsedResult = simplexml_load_string($unparsedResult);
+    
+    $retorno = array(
+        'preco' => strval($parsedResult->cServico->Valor),
+        'prazo' => strval($parsedResult->cServico->PrazoEntrega)
+    );
+    die(json_encode($retorno));
 ?>
