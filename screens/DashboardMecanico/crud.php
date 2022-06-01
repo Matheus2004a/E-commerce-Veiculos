694f7adcbd1e439103a7edba4a1af0a4ecf28194
MatheusSouzaRodrigues235 <matheus.rodrigues245@etec.sp.gov.br>
2022-05-31 22:27:38 -0300

    :feat: Demonstrando dados do usuário logado

--- screens/DashboardMecanico/crud.php
+++ screens/DashboardMecanico/crud.php
@@ -1,5 +1,10 @@
 <?php
-    session_start();
+session_start();
+include "../../connection/connection.php";
+$sql = "SELECT * FROM tbl_dados_pessoais WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . " ";
+$insert = mysqli_query($conn, $sql);
+
+$fetch = mysqli_fetch_assoc($insert);
 ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
@@ -12,127 +17,116 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Seus dados</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 
 <hr>
 <div class="container bootstrap snippet">
     <div class="row">
-  		<div class="col-sm-10"><h1><?php echo $_SESSION['username']?></h1></div>
-    	<div class="col-sm-2"><a href="../../index.php" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="../../images/icones/brand header.png"></a></div>
+        <div class="col-sm-10">
+            <h1><?php echo $_SESSION['username'] ?></h1>
+        </div>
+        <div class="col-sm-2"><a href="../../index.php" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="../../images/icones/brand header.png"></a></div>
     </div>
     <div class="row">
-  		<div class="col-sm-3"><!--left col-->
-              
+        <div class="col-sm-3">
+            <!--left col-->
 
-      <div class="text-center">
-        <img src="../../images/Usuarios/<?php echo $_SESSION['image']?>" class="avatar img-circle img-thumbnail" alt="avatar">
-        <h6>Upload a different photo...</h6>
-        <input type="file" class="text-center center-block file-upload">
-      </div></hr><br>
 
-               
-          <div class="panel panel-default">
-            <div class="panel-heading">Website <i class="fa fa-link fa-1x"></i></div>
-            <div class="panel-body"><a href="http://bootnipets.com">bootnipets.com</a></div>
-          </div>
-          
-          
-          
-               
-         
-        </div><!--/col-3-->
-    	<div class="col-sm-9">
-            <ul class="nav nav-tabs">
-                <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
-              </ul>
+            <div class="text-center">
+                <img src="../../images/Usuarios/<?php echo $_SESSION['image'] ?>" class="avatar img-circle img-thumbnail" alt="avatar">
+                <h6>Entre com uma foto diferente ....</h6>
+                <input type="file" class="text-center center-block file-upload">
+            </div>
+            </hr><br><br>
 
-              
-          <div class="tab-content">
-            <div class="tab-pane active" id="home">
-                <hr>
-                  <form class="form" action="##" method="post" id="registrationForm">
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="first_name"><h4>First name</h4></label>
-                              <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                            <label for="last_name"><h4>Last name</h4></label>
-                              <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
-                          </div>
-                      </div>
-          
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="phone"><h4>Phone</h4></label>
-                              <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
-                          </div>
-                      </div>
-          
-                      <div class="form-group">
-                          <div class="col-xs-6">
-                             <label for="mobile"><h4>Mobile</h4></label>
-                              <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Email</h4></label>
-                              <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Location</h4></label>
-                              <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                              <label for="password"><h4>Password</h4></label>
-                              <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                          
-                          <div class="col-xs-6">
-                            <label for="password2"><h4>Verify</h4></label>
-                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                           <div class="col-xs-12">
-                                <br>
-                              	<button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
-                               	<button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>
+        </div>
+        <!--/col-3-->
+        <div class="col-sm-9">
+
+
+
+            <div class="tab-content">
+                <div class="tab-pane active" id="home">
+                    <hr>
+                    <form class="form" action="./trocaDados.php" method="post" id="registrationForm">
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="first_name">
+                                    <h4>Nome</h4>
+                                </label>
+                                <input type="text" class="form-control" name="nome" id="nome" placeholder="first name" title="enter your first name if any." value="<?php echo $fetch['nome'] ?>">
                             </div>
-                      </div>
-              	</form>
-              
-              <hr>
-              
-             </div><!--/tab-pane-->
-             
-             
-               
-              </div><!--/tab-pane-->
-          </div><!--/tab-content-->
+                        </div>
 
-        </div><!--/col-9-->
-    </div><!--/row-->
-                                                      
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="phone">
+                                    <h4>Telefone</h4>
+                                </label>
+                                <input type="text" class="form-control" name="telefone" id="telefone" placeholder="enter phone" title="enter your phone number if any." value="<?php echo $fetch['telefone'] ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="email">
+                                    <h4>Email</h4>
+                                </label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email." value="<?php echo $fetch['email'] ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="cpf">
+                                    <h4>CPF</h4>
+                                </label>
+                                <input type="text" class="form-control" name="cpf" id="cpf" title="Entre com seu CPF." value="<?php echo $fetch['cpf'] ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="categoria">
+                                    <h4>Categoria</h4>
+                                </label>
+                                <input type="text" disabled class="form-control" name="categoria" id="categoria" title="Sua categoria." value="<?php echo $_SESSION['category'] ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                <br>
+                                <button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Salvar</button>
+                                <button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Resetar</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                    <hr>
+
+                </div>
+                <!--/tab-pane-->
+
+
+
+            </div>
+            <!--/tab-pane-->
+        </div>
+        <!--/tab-content-->
+
+    </div>
+    <!--/col-9-->
+</div>
+<!--/row-->
