02dc36fc480888e3fcc08b1e5440343d0db5b243
MatheusSouzaRodrigues235 <matheus.rodrigues245@etec.sp.gov.br>
2022-06-01 22:32:17 -0300

    fix Problema nas imagens do footer

--- components/footer.php
+++ components/footer.php
@@ -6,62 +6,130 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-    <footer class="footer-blog">
-        <div class="list-ordenate">
-            <h5>Contatos</h5>
-            <a href="">Lorem Ipsum</a>
-            <a href="">Lorem Ipsum</a>
-            <a href="">Lorem Ipsum</a>
-        </div>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start .bg-gradient-dark text-muted">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+                <span>Get connected with us on social networks:</span>
+            </div>
+            <!-- Left -->
 
-        <div class="list-ordenate">
-            <h5>Suporte</h5>
-            <a href="">Lorem Ipsum</a>
-            <a href="">Lorem Ipsum</a>
-            <a href="">Lorem Ipsum</a>
-        </div>
+            <!-- Right -->
+            <div>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
 
-        <div class="list-ordenate">
-            <h5>Serviços</h5>
-            <a href="">Lorem Ipsum</a>
-            <a href="">Lorem Ipsum</a>
-            <a href="">Lorem Ipsum</a>
-        </div>
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>EPS
+                        </h6>
+                        <p>
+                            Here you can use rows and columns to organize your footer content. Lorem ipsum
+                            dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
 
-        <div class="list-ordenate">
-            <h5>Pagamentos</h5>
-            <ul>
-                <li>
-                    <figure>
-                        <img src="../images/american.png" alt="icon-card-american">
-                    </figure>
-                </li>
-                <li>
-                    <figure>
-                        <img src="../images/paypal.png" alt="icon-card-paypal">
-                    </figure>
-                </li>
-                <li>
-                    <figure>
-                        <img src="../images/mastercard.png" alt="icon-card-mastercard">
-                    </figure>
-                </li>
-                <li>
-                    <figure>
-                        <img src="../images/pago.png" alt="icon-card-pago">
-                    </figure>
-                </li>
-                <li>
-                    <figure>
-                        <img src="../images/boleto.png" alt="icon-card-boleto">
-                    </figure>
-                </li>
-            </ul>
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Products
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Angular</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">React</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Vue</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Laravel</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Links Usuais
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Pricing</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Settings</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Orders</a>
+                        </p>
+                        <p>
+                            <a href="./screens/home/mapa_site.php" class="text-reset">Mapa do site</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Métodos de pagamento
+                        </h6>
+                        <i><img src="./images/american.png" alt=""></i>
+                        <i><img src="./images/visa.png" alt=""></i>
+                        <i><img src="./images/pago.png" alt=""></i>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(1, 0, 0, 0.05);">
+            © 2022 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
         </div>
+        <!-- Copyright -->
     </footer>
+    <!-- Footer -->
 </body>
 
 </html>
