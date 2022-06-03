4ff9ca7c034e15e46d00e38a8fa3a26405ff9852
MatheusSouzaRodrigues235 <matheus.rodrigues245@etec.sp.gov.br>
2022-06-03 19:47:01 -0300

    feat Removendo dados do carrinho

--- screens/compras/PagSeguro/insertPedidos.php
+++ screens/compras/PagSeguro/insertPedidos.php
@@ -1,27 +1,17 @@
 <?php 
 
-        function InsertPedidos($conn,$valItemProd,$itemQuantity1,$installmentsQty,$itemAmount)
+        function InsertPedidos($conn,$preco,$quantidade,$installmentsQty,$total)
         {
           $pedidosInsert = "INSERT INTO tbl_pedidos (data_pedido,preco_unit_prod,qtd_prod,qtd_parcelas,total_preco_prod,status_entrega,fk_id_cliente,fk_id_pagto)
           VALUES (
-            NOW(),".$valItemProd.",".$itemQuantity1.",".$installmentsQty.", ".$itemAmount.",2,".$_SESSION['idLogado'].",1
+            NOW(),".$preco.",".$quantidade.",".$installmentsQty.", ".$total.",2,".$_SESSION['idLogado'].",1
             );
         ";
+        unset($_SESSION['carrinho']);
+        unset($_SESSION['dados']);
           $insert = mysqli_query($conn,$pedidosInsert);
-          if(mysqli_query($conn,$pedidosInsert)){
-            echo "
-            <script>
-            window.onload = function() {
-                  $('#myModal').modal();
-              }
-            </script>
-            ";
-          }else{
-            echo "Erro";
-          }
+          
         }
-  
-       
        
 ?>
 
