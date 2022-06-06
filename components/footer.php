4c5e209e1312f980a6620100695063bdf897b7a3
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-06 17:48:14 -0300

    feat: Adiciona posicionamento flexbox e grid no footer

--- components/footer.php
+++ components/footer.php
@@ -5,131 +5,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <!-- Footer -->
     <footer class="text-center text-lg-start .bg-gradient-dark text-muted">
         <!-- Section: Social media -->
-        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-            <!-- Left -->
-            <div class="me-5 d-none d-lg-block">
-                <span>Get connected with us on social networks:</span>
-            </div>
-            <!-- Left -->
+        <section>
+            <p>Veja nosso trabalho desenvolvido no Github:</p>
 
             <!-- Right -->
             <div>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-google"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a href="" class="me-4 text-reset">
+                <a href="https://github.com/Matheus2004a/E-commerce-Veiculos" target="_blank" class="me-4 text-reset">
                     <i class="fab fa-github"></i>
                 </a>
             </div>
-            <!-- Right -->
         </section>
-        <!-- Section: Social media -->
 
-        <!-- Section: Links  -->
-        <section class="">
-            <div class="container text-center text-md-start mt-5">
-                <!-- Grid row -->
-                <div class="row mt-3">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                        <!-- Content -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            <i class="fas fa-gem me-3"></i>EPS
-                        </h6>
-                        <p>
-                            Here you can use rows and columns to organize your footer content. Lorem ipsum
-                            dolor sit amet, consectetur adipisicing elit.
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+        <!-- Sections Links  -->
+        <section>
+            <article>
+                <h3 class="text-uppercase fw-bold">
+                    Contatos
+                </h3>
+                <a href="mailto:teste@gmail.com">teste@gmail.com</a>
+            </article>
 
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Products
-                        </h6>
-                        <p>
-                            <a href="#!" class="text-reset">Angular</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">React</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Vue</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Laravel</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+            <article>
+                <h3 class="text-uppercase fw-bold">
+                    Links Usuais
+                    </h4>
+                    <a href="./screens/home/mapa_site.php" class="text-reset">Mapa do site</a>
+            </article>
 
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Links Usuais
-                        </h6>
-                        <p>
-                            <a href="#!" class="text-reset">Pricing</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Settings</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset">Orders</a>
-                        </p>
-                        <p>
-                            <a href="./screens/home/mapa_site.php" class="text-reset">Mapa do site</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+            <article>
+                <h3 class="text-uppercase fw-bold">
+                    Métodos de pagamento
+                </h3>
 
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Métodos de pagamento
-                        </h6>
-                        <i><img src="../../images/american.png" alt=""></i>
-                        <i><img src="../../images/visa.png" alt=""></i>
-                        <i><img src="../../images/pago.png" alt=""></i>
-                    </div>
-                    <!-- Grid column -->
+                <div class="container-cards">
+                    <figure>
+                        <img src="./images/american.png" alt="icon-american">
+                    </figure>
+                    <figure>
+                        <img src="./images/visa.png" alt="icon-visa">
+                    </figure>
+                    <figure>
+                        <img src="./images/pago.png" alt="icon-pago">
+                    </figure>
                 </div>
-                <!-- Grid row -->
-            </div>
+            </article>
         </section>
-        <!-- Section: Links  -->
 
-        <!-- Copyright -->
-        <div class="text-center p-4" style="background-color: rgba(1, 0, 0, 0.05);">
-            © 2022 Copyright:
-            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        <div class="row-footer">
+            <p>
+                &copy; 2022 Copyright:
+                <a class="text-reset fw-bold" href="#">Epa.com.br</a>
+            </p>
         </div>
-        <!-- Copyright -->
     </footer>
-    <!-- Footer -->
 </body>
 
 </html>
