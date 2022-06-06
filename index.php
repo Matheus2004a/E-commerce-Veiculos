bdcca7242f230d76ad89de048f3a23beafd112b8
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-06 17:46:41 -0300

    :lipstick: feat: Atualiza estilo da homepage

--- index.php
+++ index.php
@@ -1,175 +1,111 @@
-<!DOCTYPE HTML>
 <?php
-session_start();
-
+require "./connection/connection.php";
 ?>
-<html>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
 
 <head>
-	<title>Editorial by HTML5 UP</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="stylesheet" href="./home_alternativa/assets/css/main.css" />
-	<link rel="stylesheet" href="./home_alternativa/assets/css/background_styles.css">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<title>EPA | Home</title>
+	<link rel="stylesheet" href="./home_alternativa/assets/css/main.css">
 	<link rel="stylesheet" href="./home_alternativa/assets/css/styles.css">
+	<link rel="stylesheet" href="./components/styles/button_top_page.css">
 </head>
 
-<body class="is-preload">
-	<header>
-		<nav class="navbar">
-			<figure>
-				<img src="../images/icones/brand header.png" alt="" srcset="">
+<body>
+	<?php
+	require "./components/header.php";
+	?>
+
+	<main>
+		<section id="banner">
+			<div class="content">
+				<header>
+					<h1>EPA</h1>
+				</header>
+				<p>O projeto atual foi criado com o objetivo de facilitar ao público em geral a manutenção de seus veículos, a realização de compras e o agendamento de um mecânico. Reconhecemos que lembrar de fazer a manutenção básica do veículo tornou- se bastante difícil hoje. O sistema atual será dividido em duas fases para dois tipos de usuários: primeiro, um sistema de loja onde os usuários poderão cadastrar seus produtos, localizar sua loja e cadastrar os serviços que prestam. O usuário " loja " terá um nível de acesso superior ao usuário "comum" e o usuário "comum" será a base do sistema</p>
+				<button class="button big">Saiba mais</button>
+			</div>
+			<figure class="image object">
+				<img src="./home_alternativa/images/pic10.jpg" alt="image-lamborghini">
 			</figure>
-			<a href="#" class="toggle-button">
-				<span class="bar"></span>
-				<span class="bar"></span>
-				<span class="bar"></span>
-			</a>
-			<div class="navbar-links">
-				<ul>
-					<div id="google_translate_element"></div>
-					<li><a href="##">Home</a></li>
-					<li><a href="##">Sobre</a></li>
-					<li><a href="./screens/login/index.php">Login</a></li>
-					<li><a href="./screens/compras/index.php">Compras</a></li>
-					<li><a href="./screens/listService/index.php">Serviços</a></li>
-					<li><a href="./screens/login/logout.php">Sair</a></li>
-				</ul>
-			</div>
-		</nav>
-	</header>
+		</section>
 
-	<!-- Wrapper -->
-	<div id="wrapper">
-		<!-- Main -->
-		<div id="main">
-			<div class="inner">
-				<!-- Banner -->
-				<section id="banner">
+		<section>
+			<header class="major" id="##">
+				<h2>Nossos Serviços</h2>
+			</header>
+			<div class="features">
+				<article>
+					<span class="icon solid fa-comment"></span>
 					<div class="content">
-						<header>
-							<h1>FMS<br />
-							</h1>
-							<p>Completamente Responsiva!</p>
-						</header>
-						<p>O projeto atual foi criado com o objetivo de facilitar ao público em geral a manutenção de seus veículos, a realização de compras e o agendamento de um mecânico. Reconhecemos que lembrar de fazer a manutenção básica do veículo tornou- se bastante difícil hoje. O sistema atual será dividido em duas fases para dois tipos de usuários: primeiro, um sistema de loja onde os usuários poderão cadastrar seus produtos, localizar sua loja e cadastrar os serviços que prestam. O usuário " loja " terá um nível de acesso superior ao usuário "comum" e o usuário "comum" será a base do sistema</p>
-						<ul class="actions">
-							<li><a href="#" class="button big">Learn More</a></li>
-						</ul>
+						<h3>Chat em tempo Real</h3>
+						<p>Converse em tempo real com seu cliente ou mecânico, confirme informações, agendamentos, tire dúvidas com seus clientes e mecânicos!</p>
 					</div>
-					<span class="image object">
-						<img src="./home_alternativa/images/pic10.jpg" alt="" />
-					</span>
-				</section>
-
-				<!-- Section -->
-				<section>
-					<header class="major" id="##">
-						<h2>Nossos Serviços</h2>
-					</header>
-					<div class="features">
-						<article>
-							<span class="icon fa-gem"></span>
-							<div class="content">
-								<h3>Chat em tempo Real</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-paper-plane"></span>
-							<div class="content">
-								<h3>Agendamento de serviços</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-rocket"></span>
-							<div class="content">
-								<h3>Compra e venda</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-signal"></span>
-							<div class="content">
-								<h3>Um e-commerce completo</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
+				</article>
+				<article>
+					<span class="icon solid fa-calendar"></span>
+					<div class="content">
+						<h3>Agendamento de serviços</h3>
+						<p>Agende seus serviços com mecânicos sem sair do conforto de sua casa, e combine com seu mecânico sistemas de leva e traz.</p>
 					</div>
-				</section>
-
-				<!-- Section -->
-				<section>
-					<header class="major">
-						<h2>Produtos Mais Vendidos</h2>
-					</header>
-					<div class="posts">
-						<article>
-							<a href="#" class="image"><img src="./home_alternativa/images/pic01.jpg" alt="" /></a>
-							<h3>Interdum aenean</h3>
-							<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-								facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							<ul class="actions">
-								<li><a href="#" class="button">More</a></li>
-							</ul>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="./home_alternativa/images/pic02.jpg" alt="" /></a>
-							<h3>Nulla amet dolore</h3>
-							<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-								facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							<ul class="actions">
-								<li><a href="#" class="button">More</a></li>
-							</ul>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="./home_alternativa/images/pic03.jpg" alt="" /></a>
-							<h3>Tempus ullamcorper</h3>
-							<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-								facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							<ul class="actions">
-								<li><a href="#" class="button">More</a></li>
-							</ul>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="./home_alternativa/images/pic04.jpg" alt="" /></a>
-							<h3>Sed etiam facilis</h3>
-							<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-								facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							<ul class="actions">
-								<li><a href="#" class="button">More</a></li>
-							</ul>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="./home_alternativa/images/pic05.jpg" alt="" /></a>
-							<h3>Feugiat lorem aenean</h3>
-							<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-								facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							<ul class="actions">
-								<li><a href="#" class="button">More</a></li>
-							</ul>
-						</article>
-						<article>
-							<a href="#" class="image"><img src="./home_alternativa/images/pic06.jpg" alt="" /></a>
-							<h3>Amet varius aliquam</h3>
-							<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-								facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							<ul class="actions">
-								<li><a href="#" class="button">More</a></li>
-							</ul>
-						</article>
+				</article>
+				<article>
+					<span class="icon solid fa-comments-dollar"></span>
+					<div class="content">
+						<h3>Compra e venda</h3>
+						<p>Compre peças para seus veiculos com fornecedores de todo o Brasil e receba em sua casa e pague com cartão de crédito ou boleto.</p>
 					</div>
-				</section>
+				</article>
+				<article>
+					<span class="icon solid fa-store"></span>
+					<div class="content">
+						<h3>Um e-commerce completo</h3>
+						<p>Criado com o objetivo de facilitar a manutenção de seus veículos, compras de peças e agendamento de serviços para realizar manutenções em seu veículo, que tornou- se bastante difícil atualmente, trazendo assim praticidade.</p>
+					</div>
+				</article>
 			</div>
-		</div>
-	</div>
+		</section>
 
-	<!-- Scripts -->
+		<section>
+			<header class="major">
+				<h2>Conheça nossos produtos</h2>
+			</header>
+			<div class="posts">
+				<?php
+				$sql = "SELECT `id_prod`, `nome_prod`, `categoria_prod`, `preco_custo_prod` AS preco, `desc_prod`, `foto_prod`, `qtd_estoque` FROM `tbl_produtos` LIMIT 6";
+				$result = mysqli_query($conn, $sql);
+
+				while ($row = mysqli_fetch_assoc($result)) {
+				?>
+					<article>
+						<a href="#" class="image">
+							<img src="./home_alternativa/images/pic01.jpg" alt="<?php $row['nome_prod'] ?>">
+						</a>
+						<h3><?php echo $row['nome_prod'] ?></h3>
+						<h4><?php echo $row['desc_prod'] ?></h4>
+						<p><?php echo "R$ " . number_format($row['preco'], 2, ",", ".") ?></p>
+						<button class="button">Veja mais</button>
+					</article>
+				<?php
+				}
+				?>
+			</div>
+		</section>
+
+		<?php
+		require "./components/button_top_page.php";
+		?>
+	</main>
+
+	<?php
+	require "./components/footer.php";
+	?>
+
+	<script src="./screens/home/js/main.js"></script>
 	<script src="./components/traducao.js"></script>
 	<script src="./home_alternativa/assets/js/jquery.min.js"></script>
 	<script src="./home_alternativa/assets/js/browser.min.js"></script>
