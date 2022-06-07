6ff2c74a32de6ba5208796f02b82f3aaf8eac829
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-07 14:57:17 -0300

    :heavy_plus_sign: build: Adiciona lib fontawesome

--- index.php
+++ index.php
@@ -114,6 +114,7 @@ require "./connection/connection.php";
 	<script src="./home_alternativa/assets/js/main.js"></script>
 	<script src="./home_alternativa/assets/js/script.js"></script>
 	<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+	<script src="./libs/fontawesome/fontawesome.js"></script>
 </body>
 
 </html>
