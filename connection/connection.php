d9b6f388299c2be19c5840a278d8f21730f364c4
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-08 20:29:33 -0300

    :pencil: Remove comentários da conexão

--- connection/connection.php
+++ connection/connection.php
@@ -1,29 +1,12 @@
 <?php
-//bdtcc.mysql.database.azure.com
-//UserPadrao
-//kGz6o&dvjHzL%YI
 $server_name = "localhost";
 $username = "root";
 $password = "";
 $database_name = "bd_veiculos_tcc";
-
 
 $conn = mysqli_connect($server_name, $username, $password, $database_name);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-// Run the create table query
-
-
-
-
-//Close the connection
-
-
-
-
-
-
-
 ?>
