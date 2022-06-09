4c25a862a1d3fa62ed51f1c3a175b1f7ba599228
ImBard <nicota1234@gmail.com>
2022-06-09 20:10:27 -0300

    :recycle: Formatando código, sql query e sessão image

--- screens/login/login.php
+++ screens/login/login.php
@@ -6,7 +6,7 @@
 	$password = mysqli_real_escape_string($conn, $_POST['senha']);
 
 	if (isset($_POST['email']) && isset($_POST['senha'])) {
-		$sql = "SELECT id_dados_pessoais,`nome`, `email`, `senha`, `categoria`,foto_perfil FROM `tbl_dados_pessoais` WHERE email = '$email'";
+		$sql = "SELECT `id_dados_pessoais`, `nome`, `email`, `senha`, `categoria`, `foto_perfil` FROM `tbl_dados_pessoais` WHERE email = '$email'";
 		$result = mysqli_query($conn, $sql);
 		
 		if ($result) {
@@ -27,10 +27,10 @@
 	function compare_password_with_hash($password, $row){
 		// Verifica se a senha digitada é igual a que está criptografada no banco
 		if (password_verify($password, $row['senha'])) {
+			$_SESSION['image'] = $row['foto_perfil'];
 			$_SESSION['idLogado'] = $row['id_dados_pessoais'];
 			$_SESSION['username'] = $row['nome'];
 			$_SESSION['category'] = $row['categoria'];
-			$_SESSION['image'] = $row['foto_perfil'];
 			header('location: ../../index.php');
 		} else {
 			$_SESSION['no-authenticated'] = "<div class='alert alert-danger d-flex align-items-center' role='alert'>
