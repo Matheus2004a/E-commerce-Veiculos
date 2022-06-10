d5e5eeb23687eee8318d567dc40c55c405fe8041
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-10 02:22:31 -0300

    :ok_hand: style: Adiciona ícone e deleta imagem de usuário

--- components/header.php
+++ components/header.php
@@ -1,13 +1,16 @@
+<?php session_start() ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
-<?php session_start() ?>
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/../E-commerce-Veiculos/home_alternativa/assets/css/main.css">
-    <link rel="stylesheet" href="/../E-commerce-Veiculos/home_alternativa/assets/css/styles.css"> 
+    <link rel="stylesheet" href="/../E-commerce-Veiculos/home_alternativa/assets/css/styles.css">
+    <link rel="stylesheet" href="../screens/requests/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
@@ -25,21 +28,19 @@
             <ul>
                 <li><a href="/../E-commerce-veiculos/">Home</a></li>
                 <li><a href="/../E-commerce-veiculos/">Sobre</a></li>
+                <li><a href="/../E-commerce-veiculos/screens/listService/">Serviços</a></li>
+                <li><a href="/../E-commerce-veiculos/screens/compras/">Compras</a></li>
 
-                <li><a href="/../E-commerce-veiculos/screens/listService/index.php">Serviços</a></li>
-                <li><a href="/../E-commerce-veiculos/screens/compras/index.php">Compras</a></li>
                 <?php
-
                 if (isset($_SESSION['category']) && $_SESSION['category'] == "mecânico") {
-                        echo "<li><a href='/../E-commerce-veiculos/screens/CadastrarServicos/index.php'>Cadastrar Serviço</a></li>";
+                    echo "<li><a href='/../E-commerce-veiculos/screens/CadastrarServicos/'>Cadastrar Serviço</a></li>";
                 }
-
 
                 if (isset($_SESSION['username']) && $_SESSION['username']) { ?>
                     <li><a href="/../E-commerce-veiculos/screens/login/logout.php">Sair</a></li>
                 <?php
                 } else { ?>
-                    <li><a href="/../E-commerce-veiculos/screens/login/index.php">Login</a></li>
+                    <li><a href="/../E-commerce-veiculos/screens/login/">Login</a></li>
                 <?php
                 }
                 ?>
@@ -47,12 +48,17 @@
                 <?php
                 if (isset($_SESSION['image'])) {
                     if ($_SESSION['image'] != null) {
-                        echo '<a href="/../E-commerce-veiculos/screens/DashboardMecanico/index.php"> <li> <img src="/../E-commerce-veiculos/images/users/' . $_SESSION['image']  . '" class="img_usuario" alt=""></li> </a>';
+                        echo '<a href="/../E-commerce-veiculos/screens/DashboardMecanico/">
+                        <li>
+                            <img src="/../E-commerce-veiculos/images/users/' . $_SESSION['image']  . '" class="img_usuario" alt="">
+                        </li>
+                        </a>';
                     } else {
-                        echo '<a href="../DashboardMecanico/index.php"> <li> <img src="/../E-commerce-veiculos/images/users/default-user.png" class="img_usuario" alt=""></li> </a>';
+                        echo '<a href="/../E-commerce-Veiculos/screens/DashboardMecanico/">
+                        <i class="bx bxs-user-circle icon-user"></i>
+                        </a>';
                     }
                 }
-
                 ?>
             </ul>
         </nav>
