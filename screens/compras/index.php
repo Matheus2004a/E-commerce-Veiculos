501754df290213f370b00aadc4eb0ea091cac6c0
ImBard <nicota1234@gmail.com>
2022-06-10 01:28:21 -0300

    :recycle: Formatando e identando corretamento o código

--- screens/compras/index.php
+++ screens/compras/index.php
@@ -14,7 +14,8 @@ session_start();
 	<link rel="stylesheet" href="../../bootstrap-5.1.3-dist/css/bootstrap.min.css">
 	<script src="https://cdn.tailwindcss.com"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="../home/css/style.css">
+	<link rel="stylesheet" href="../../home_alternativa/assets/css/main.css">
+	<link rel="stylesheet" href="../../home_alternativa/assets/css/styles.css">
 	<link rel="stylesheet" href="../../components/styles/button_top_page.css">
 </head>
 
@@ -37,8 +38,8 @@ session_start();
 	}
 	$result_query = mysqli_query($conn, $sql);
 
-	
-	
+
+
 	?>
 
 	<a name="top-page"></a>
@@ -47,7 +48,7 @@ session_start();
 	?>
 
 	<main>
-		<div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+		<div class="max-w-2xl mx-auto py-0 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 			<div class="flex items-center justify-between mb-8">
 				<form class="w-1/3" action="" method="POST">
 					<input class="form-control" type="search" placeholder="Pesquise aqui o que você procura..." aria-label="Search" name="search">
@@ -67,9 +68,9 @@ session_start();
 			<h2 class="mb-4 text-2xl font-bold">Produtos</h2>
 			<article class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 				<?php
-				
-						
-				
+
+
+
 				if (mysqli_num_rows($result_query) > 0) {
 					while ($row = mysqli_fetch_assoc($result_query)) {
 						if ($row['qtd_estoque'] > 0) {
@@ -77,26 +78,24 @@ session_start();
 						} else {
 							$_SESSION['status_estoq'] = "<span class='text-xs text-red-600 bg-red-100 p-1 rounded'>Indisponível</span>";
 						}
-						
-						
+
+
 						echo "<div class='group shadow-md rounded-lg'>
 						<a href='desc-product.php?id=" . $row['id_prod'] . "'>
 								<div class='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-t-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
 									<figure>
-										<img src=' " . $row['foto_prod'] . "' id='ImageProd' value='".$row['foto_prod']."' alt='' class='w-full h-60 object-center object-cover group-hover:opacity-75'>
+										<img src=' " . $row['foto_prod'] . "' id='ImageProd' value='" . $row['foto_prod'] . "' alt='' class='w-full h-60 object-center object-cover group-hover:opacity-75'>
 									</figure>
 								</div>
 								<div class='p-3'>
 									<h3 class='text-base text-gray-700 truncate' id='nomeProduto' value='11'>" . $row['nome_prod'] . "</h3>
-									<p class='flex items-center justify-between mt-1 text-lg font-medium text-gray-900' value='".$row['preco']."' id='precoProduto'>R$ " . $row['preco'] . "" . $_SESSION['status_estoq'] . "</p>
+									<p class='flex items-center justify-between mt-1 text-lg font-medium text-gray-900' value='" . $row['preco'] . "' id='precoProduto'>R$ " . $row['preco'] . "" . $_SESSION['status_estoq'] . "</p>
 
-										<button type='button' class='bg-slate-500 w-100 mt-2 btn btn-secondary' onclick='saveData()' ><a href='./cart/template_store/cart.php?add=carrinho&id_prod=".$row['id_prod']."'> Adicionar ao carrinho </a> </button>
+										<button type='button' class='bg-slate-500 w-100 mt-2 btn btn-secondary' onclick='saveData()' ><a href='./cart/template_store/cart.php?add=carrinho&id_prod=" . $row['id_prod'] . "'> Adicionar ao carrinho </a> </button>
 									</a>
 								</div>
 							</a>
 							</div>";
-							
-							
 					}
 				} else {
 					echo "<div class='alert alert-warning flex items-center w-full' role='alert'>
@@ -106,15 +105,15 @@ session_start();
 									</div>
 								  </div>";
 				}
-				
+
 				mysqli_close($conn);
 				?>
 			</article>
 		</div>
 
-		<?php
-			require "../../components/button_top_page.php"
-		?>
+		<a href="#top-page">
+			<button class="button-top-page bg-sky-500"><i class="fas fa-arrow-up"></i></button>
+		</a>
 	</main>
 
 	<?php require_once "../../components/footer.php"; ?>
@@ -126,21 +125,19 @@ session_start();
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.min.js"></script>
 	<script>
-		function saveData(){
-			if(typeof(Storage)!== "undefined"){
-				if(localStorage.cont){
-					localStorage.cont = Number(localStorage.cont)+1;
-				}else{
+		function saveData() {
+			if (typeof(Storage) !== "undefined") {
+				if (localStorage.cont) {
+					localStorage.cont = Number(localStorage.cont) + 1;
+				} else {
 					localStorage.cont = 1;
 				}
 				var cad;
 				cad = document.getElementById('ImageProd').value + ';' + document.getElementById('nomeProduto').value + ';' + document.getElementById('precoProduto').value;
-				localStorage.setItem("cad_"+localStorage.cont,cad);
-				
+				localStorage.setItem("cad_" + localStorage.cont, cad);
+
 			}
-		}		
-		
-		
+		}
 	</script>
 </body>
 
