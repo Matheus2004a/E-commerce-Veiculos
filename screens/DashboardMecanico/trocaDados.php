db3f3f0dcf520fc06b01efce1409b0e9704041ba
ImBard <nicota1234@gmail.com>
2022-06-11 00:24:03 -0300

    :construction: Adiciona slq de consulta pra alterar os dados da sessão

--- screens/DashboardMecanico/trocaDados.php
+++ screens/DashboardMecanico/trocaDados.php
@@ -8,14 +8,13 @@ $email = mysqli_real_escape_string($conn, $_POST['email']);
 $cpf = mysqli_real_escape_string($conn, $_POST['cpf']);
 
 $sql = "UPDATE tbl_dados_pessoais SET nome = '$nome', cpf = '$cpf', email = '$email', telefone = '$telefone' WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . "";
-$select = "SELECT `id_dados_pessoais`, `cpf`, `nome`, `email`, `senha`, `telefone`, `categoria`, `foto_perfil` FROM `tbl_dados_pessoais` WHERE WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . "";
+$select = "SELECT `id_dados_pessoais`, `cpf`, `nome`, `email`, `senha`, `telefone`, `categoria`, `foto_perfil` FROM `tbl_dados_pessoais` WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . "";
 
-if ($result_query = mysqli_query($conn, $select)) {
-    unset($_SESSION['username']);
-    $rows_result = mysqli_fetch_assoc($result_query);
+$result_query = mysqli_query($conn, $select);
+$rows_result = mysqli_fetch_assoc($result_query);
+
+if (mysqli_query($conn, $sql)) {
     $_SESSION['username'] = $rows_result['nome'];
-
-    echo "<script>alert(". $_SESSION['username'].")</script>";
 
     $_SESSION['success'] = "<div class='alert alert-success' role='alert'>
     Dados atualizado com sucesso !!!
