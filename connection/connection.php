0f7f101f91b4078ef31aa6acacc5241cf842efcf
ImBard <nicota1234@gmail.com>
2022-06-11 00:27:52 -0300

    connection

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,10 @@
 <?php
-$server_name = "banco-epa-remoto.mysql.database.azure.com";
-$username = "UserPadrao_EPA";
-$password = "vRZb%8T7tguZ";
+$server_name = "localhost";
+$username = "root";
+$password = "";
 $database_name = "bd_veiculos_tcc";
 
-$conn = mysqli_connect($server_name, $username, $password, $database_name);
+$conn = mysqli_connect($server_name, $username, $password, $database_name, 3307);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
