f44e8ffeb72faa2564992fe6beb2b0f3909b8d9a
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-10 21:45:48 -0300

    :construction: Tenta sobrescrever sessão nome do usuário

--- screens/DashboardMecanico/trocaDados.php
+++ screens/DashboardMecanico/trocaDados.php
@@ -1,41 +1,30 @@
 <?php
 session_start();
-include "../../connection/connection.php";
+require "../../connection/connection.php";
 
-$nome = $_POST['nome'];
-$telefone = $_POST['telefone'];
-$email = $_POST['email'];
-$cpf = $_POST['cpf'];
+$nome = mysqli_real_escape_string($conn, $_POST['nome']);
+$telefone = mysqli_real_escape_string($conn, $_POST['telefone']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$cpf = mysqli_real_escape_string($conn, $_POST['cpf']);
 
-$sql = "UPDATE tbl_dados_pessoais SET nome = '.$nome.', cpf = '.$cpf.', email = '.$email.', telefone = '.$telefone.' WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . " ";
+$sql = "UPDATE tbl_dados_pessoais SET nome = '$nome', cpf = '$cpf', email = '$email', telefone = '$telefone' WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . "";
+$select = "SELECT `id_dados_pessoais`, `cpf`, `nome`, `email`, `senha`, `telefone`, `categoria`, `foto_perfil` FROM `tbl_dados_pessoais` WHERE WHERE id_dados_pessoais = " . $_SESSION['idLogado'] . "";
 
+if ($result_query = mysqli_query($conn, $select)) {
+    unset($_SESSION['username']);
+    $rows_result = mysqli_fetch_assoc($result_query);
+    $_SESSION['username'] = $rows_result['nome'];
 
+    echo "<script>alert(". $_SESSION['username'].")</script>";
+
+    $_SESSION['success'] = "<div class='alert alert-success' role='alert'>
+    Dados atualizado com sucesso !!!
+    </div>";
+    header("location: crud.php");
+} else {
+    $_SESSION['error'] = "<div class='alert alert-danger' role='alert'>
+    Erro ao atualizar dados !!!
+    </div>";
+    header("location: crud.php");
+}
 ?>
-<html>
-
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
-
-<body>
-    <?php
-    if (mysqli_query($conn, $sql)) {
-        echo '
-        <div class="alert alert-success" role="alert">
-         Atualizado com sucesso !!!
-        </div>';
-    } else {
-        echo '
-        <div class="alert alert-danger" role="alert">
-            Erro ao atualizar !!!
-        </div>';
-    }
-
-    ?>
-</body>
-    <script>
-        setTimeout(function(){
-            window.location.href('../index.php');
-        },5000);
-    </script>
-</html>
