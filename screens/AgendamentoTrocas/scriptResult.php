926762e8154bfef0eb5685c48987e464dfbd6b39
ImBard <nicota1234@gmail.com>
2022-06-16 20:12:47 -0300

    :bug: fix: Adiciona observações no scrfipt

--- screens/AgendamentoTrocas/scriptResult.php
+++ screens/AgendamentoTrocas/scriptResult.php
@@ -14,7 +14,7 @@
     $resultado = mysqli_query($conn, $sql_id_cliente);
     $row_id = mysqli_fetch_assoc($resultado);
 
-    $sql = "INSERT INTO `bd_veiculos_tcc`.`tbl_agendamentos_servicos` (`data_agend`, `hora_agend`, `fk_id_cliente`, `fk_id_servico`) VALUES ('$data', '$hora', '".$row_id['id_cliente']."'']', '$id_service')";
+    $sql = "INSERT INTO `bd_veiculos_tcc`.`tbl_agendamentos_servicos` (`data_agend`, `hora_agend`, `obs_servico`, `fk_id_cliente`, `fk_id_servico`) VALUES ('$data', '$hora', '$obs','".$row_id['id_cliente']."'']', '$id_service')";
 
     if (mysqli_query($conn, $sql)) {
 
