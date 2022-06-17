52cef1599b514adce173b10753ea27cb9f87730a
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-06-16 22:24:18 -0300

    :goal_net: Valida upload para cadastrar produto

--- screens/register-products/upload.php
+++ screens/register-products/upload.php
@@ -38,7 +38,7 @@ if (isset($_FILES['file'])) {
 
 function register_product($conn, $sql, $file, $path_file)
 {
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($conn, $sql) && move_uploaded_file($file['tmp_name'], $path_file)) {
         $_SESSION['register-product'] = "<div class='alert alert-success d-flex align-items-center'role='alert'>
             <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'><use xlink:href='#check-circle-fill'/></svg>
             <div>
